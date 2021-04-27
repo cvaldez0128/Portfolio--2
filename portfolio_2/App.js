1b2d0f2f2ea48dd4115f157d19c260168216a07d
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-26 20:35:08 -0400

    April 26, 2021 8:35 PM

--- portfolio_2/App.js
+++ portfolio_2/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView, Button, View, TextInput } from 'react-native';
 import { Card } from 'react-native-elements';
-import { createDrawerNavigator } from '@reacr-navigation/drawer';
-import { NavigationContainer } from '@reacr-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 let todoList = [
   {
