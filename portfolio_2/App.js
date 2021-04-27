f031049ce3da2efe300cce7f6336a76ce6a11746
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-26 20:50:22 -0400

    April 26, 2021 8:50 PM

--- portfolio_2/App.js
+++ portfolio_2/App.js
@@ -1,108 +1,156 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
-import { Overlay, Input } from 'react-native-elements';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import PropTypes from 'prop-types';
-import TodoList from './components/Todo'
-import Modal from 'modal-react-native-web';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, Button, View, TextInput } from 'react-native';
+import { Card } from 'react-native-elements';
 
-function HomeScreen({ navigation }) {
-  const [showLogin, setShowLogin] = useState(false)
-  const [login, setLogin] = useState(false)
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  return (
-    <View style={styles.body}>
-      {!login ?
-        <>
+let todoList = [
+  {
+    key: "1",
+    description: "Volunteer in an important political compaign",
+    completed: true
+  },
+  {
+    key: "2",
+    description: "Study all the works of William Shakespeare",
+    completed: true
+  },
+  {
+    key: "3",
+    description: "Learn to play piano",
+    completed: true
+  },
+  {
+    key: "4",
+    description: "Speak fluent French",
+    completed: true
+  },
+  {
+    key: "5",
+    description: "Visit the Sistine Chapal with Abby",
+    completed: true
+  },
+]
 
-          <Text style={styles.text}>Welcome! Please Log In Below</Text>
-    
-          <Button
-            style={styles.buttonStyle}
-            onPress={() => setShowLogin(true)}
-            title="Log In"
-          />
-        </>
-        : 
-        <>
-            <Text style={styles.text}>Welcome {username}</Text>
-            <Button 
-              style={styles.buttonStyle}
-              onPress={() => setLogin(false)}
-              title="Log Out"
-            />
-        </>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    let inputRef = React.createRef()
+    let checkRef = React.createRef()
+    this.state = {todoList,curKey:6,inputRef, checkRef}
+  }
 
-      }
 
-      <Button
-        style={styles.buttonStyle}
-        onPress={() => navigation.openDrawer()}
-        title="Open Left Drawer"
-      />
 
-      <Overlay isVisible={showLogin} ModalComponent={Modal}>
-        <Text style={styles.Logintext}>Please Log In Below</Text>
-        <Input title="Username" value={username} onChangeText={(un => setUsername(un))}></Input>
-        <Input title="Password" value={password} secureTextEntry={true} onChangeText={(pw) => setPassword(pw)}></Input>
-        <Button title="Log In" onPress={() => { setLogin(true); setShowLogin(false)}}></Button>
-      </Overlay>
+  completeItem = (itemKey) => {
+    this.setState((prevState) => {
+      let prevList = prevState.todoList
+      let itemIndex = prevList.findIndex((item) => item.key == itemKey)
+      prevList[itemIndex].completed = !prevList[itemIndex].completed
+      return {todoList:prevList}
+    })
+  }
 
-    </View>
-  );
+  handleKeyPress = (event) => {
+    if (event.key == "Enter") {
+      this.addTodoItem()
+     
+    }
+  }
+
+  
+  addTodoItem = () => {
+    let prevValue = this.state.inputRef.current.value
+    this.setState(prevState => ({
+      todoList: [...prevState.todoList,
+      { completed: false, description: prevValue, key: prevState.curKey }],
+      curKey: prevState.curKey+1
+    }))
+    this.state.inputRef.current.value = ""
+  }  
+  
+  render() {
+    return (
+      <>
+        <View style={styles.container}>
+            <Card>
+                <SafeAreaView style={[styles.container, styles.text]}>
+                    <Text style={styles.hText}>TO-DO LIST{"\n"}</Text>
+                    <Card.Divider></Card.Divider>
+                    
+                    {this.state.todoList.map((item) => (
+                      <View style={styles.label}>
+                        
+                          <Text style={styles.numbers}>{item.key}</Text>
+                          
+                          <input 
+                              ref={this.state.checkRef} 
+                              type="checkbox" 
+                              onChange={() => this.completeItem(item.key)} 
+                              defaultChecked={item.completed}>
+                          </input>
+                            
+                          <Text style={[styles.text, item.completed ? {textDecoration: "line-through"} : undefined]}>{item.description}</Text>
+                        
+                      </View>
+                    ))}
+                        
+                      
+                      
+                    <TextInput style={styles.textInput} onKeyPress={(event) => this.handleKeyPress(event)} ref={this.state.inputRef}></TextInput>
+                    <Button onPress={() => this.addTodoItem()} title="Add new item"></Button>
+                </SafeAreaView>
+            </Card>
+
+           
+        </View>                                                 
+       
+      </>
+    );
+  }
+
 }
 
-HomeScreen.propTypes = {
-    navigation: PropTypes.any
-}
-
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-NotificationsScreen.propTypes = {
-    navigation: PropTypes.any
-}
-
-const Drawer = createDrawerNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      
-    </NavigationContainer>
-  );
-}
-        
 const styles = StyleSheet.create({
-  text: {
-    textAlign: "center",
-    fontFamily: "Artico Light",
-    fontSize: 20,
-    color: "white",
-  },
-  Logintext: {
-    textAlign: "center",
-    fontFamily: "Artico Light",
-    
-  },
-  body: {
-    backgroundColor: "#213479",
-    border: "15px solid black",
+  container: {
     flex: 1,
+    backgroundColor: '#6e202e',
+    borderWidth: 15,
     alignItems: "center",
-    
   },
-    buttonStyle: {
-    flex: 1,
-    padding: 2
+  card: {
+    backgroundColor: '#6e202e',
   },
+  text: {
+    color: "white",
+    fontFamily: "Artico Light",
+    textAlign: "center",
+    fontSize: "20px",
+  },
+  numbers: {
+    fontFamily: "Artico Light",
+    color: "white",
+    fontSize: 20,
+  },
+  hText: {
+    color: "white",
+    fontFamily: "Artico Light",
+    textAlign: "center",
+    fontSize: 30,
+  },
+  button: {
+    textAlign: "center",
+  },
+  textInput: {
+    height: 20,
+    width: 200,
+    marginBottom: 12,
+    marginLeft: 12,
+    marginRight: 12,
+    borderWidth: 2,
+    backgroundColor: "white",
+  },
+  label: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  
 });
