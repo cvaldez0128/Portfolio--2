93dd335b206c19e715e53833e9947d01147fff99
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-27 23:30:32 -0400

    April 27, 2021 11:30 PM

--- portfolio_2/components/ImageGallery/index.js
+++ portfolio_2/components/ImageGallery/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View, TouchableWithoutFeedback, Dimensions, Modal } from 'react-native';
+import ImageElement from './components/ImageElement';
+
 
 export default class ImageGallery extends React.component {
     render() {
