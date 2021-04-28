7a03610f1608a5cee9b1db836d22c0df80733d06
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-27 23:45:26 -0400

    April 27, 2021 11:45 PM

--- portfolio_2/components/ImageGallery/index.js
+++ portfolio_2/components/ImageGallery/index.js
@@ -28,7 +28,9 @@ export default class ImageGallery extends React.component {
     render() {
 
         let images = this.state.images.map((val, key) => {
-            return <TouchableWithoutFeedback key
+            return <TouchableWithoutFeedback key={key}
+                        onPress={() =>}
+                    ></TouchableWithoutFeedback>
         });
 
         return (
