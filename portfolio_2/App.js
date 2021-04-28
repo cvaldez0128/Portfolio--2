79db29ace73e2f78b1937d5d84e47765dcd4a11f
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-27 22:28:37 -0400

    April 27, 2021 10:28 PM

--- portfolio_2/App.js
+++ portfolio_2/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import { Card } from 'react-native-elements';
 import Todo from './components/todo';
 import Date from './components/DatePicker';
-import { render } from 'react-dom';
+
 
 const USER = "cvaldez2218"
 
