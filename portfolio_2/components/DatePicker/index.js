ec34b430f746599b17643d9013522fb3e2501fab
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 12:23:45 -0400

    April 28, 2021 12:23 PM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -1,65 +1,20 @@
 import React from 'react';
-import { useState, useRef } from 'react'
-import { StyleSheet, Text, SafeAreaView, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, TextInput, Button } from 'react-native';
 import { Card } from 'react-native-elements';
-
-function TestApp () {
-    return (
-        <>
-            <Text>This is a test</Text>
-        </>
-    )
-}
 
 export default class Date extends React.Component {
 
     render() {
-        let [accessIn, setAccessIn] = useState(false)
-        let codeRef = useRef(null)
-    
-        let access = () => {
-            if (passRef.current.value == "vaccine") {
-                setAccessIn(true)
-                console.log("Access granted")
-            }
-        }
         
-
+      
         return (
             <>
-
-                {accessIn ?
-                    <>
-                        <TestApp></TestApp>
-                    </>
-                : undefined}
-
-
-            {!accessIn ?
+    
                <Card>
                    <SafeAreaView style={styles.container}>
-                       <Text style={styles.header}>THE NEXT COMPONENT IS PASSWORD PROTECTED</Text>
-                       <Card.Divider/>
-                       <Text style={styles.text}>
-                            Hint: Getting this is entirely optional. 
-                            Some are waiting, some have already gotten it. 
-                            It will prevent the spread.
-                            One word, all lowercase.
-                        </Text>
-                        <Card.Divider/>
-                        <TextInput
-                            style={styles.textInput}
-                            ref={codeRef}
-                            placeholder="Did you guess the code word?"
-                            secureTextEntry={true}
-                            ></TextInput>
-                        <Button 
-                            onPress={access}
-                            title="Enter"
-                            />
+                      
                    </SafeAreaView>
                </Card>
-            : undefined}
             </>
         );
     }
