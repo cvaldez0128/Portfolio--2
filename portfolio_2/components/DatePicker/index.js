9ca84dc0b36ce4590e3a2fdb80f6a10835e296da
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 12:27:43 -0400

    April 28, 2021 12:27 PM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -12,7 +12,7 @@ export default function passWord() {
     
                <Card>
                    <SafeAreaView style={styles.container}>
-                    >
+                    
                    </SafeAreaView>
                </Card>
             </>
