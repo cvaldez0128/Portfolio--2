ac3628e10b6d2f868e2c08ead89b80eef81bc668
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 02:13:07 -0400

    April 28, 2021 2:13 AM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -2,37 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Text, SafeAreaView, TextInput, Button } from 'react-native';
 import { Card } from 'react-native-elements';
 
-function Quiz () {
-    var favFruit = prompt("What is your favorite fruit?", "");
 
-    var favMovie = prompt("What is your favorite movie?", "");
-
-    var strInput = prompt("Please enter 8, 9, or 10");
-    var intInput = parseInt(strInput);
-
-    switch (intInput) {
-    case 8:
-        alert("You entered 8!");
-        break;
-    case 9:
-        alert("You entered 9!");
-        break;
-    case 10:
-        alert("You entered 10!");
-        break;
-    default:
-        alert("I said a number between 8 and 10");
-    }
-    intInput = String(intInput);
-    alert(
-    "You said your favorite fruit is: " +
-        favFruit +
-        ". Your favorite movie is: " +
-        favMovie +
-        " and you chose: " +
-        intInput
-    );
-}
 
 export default class Date extends React.Component {
     constructor(props) {
@@ -44,7 +14,8 @@ export default class Date extends React.Component {
     render() {
         return (
             <>
-               
+
+                <Text style={Styles.}
               
             </>
                     
