252481b1ad83d9102f169715631e67a49b3a323a
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 11:18:44 -0400

    April 28, 2021 11:18 AM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -103,6 +103,7 @@ export default class Date extends React.Component {
                             </div>
 
                        </Text>
+                       
                    </SafeAreaView>
                </Card>
             </>
