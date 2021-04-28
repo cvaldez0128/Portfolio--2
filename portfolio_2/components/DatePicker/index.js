81d583a664cf68bc4253bbe5181aca214efc15a9
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 10:49:14 -0400

    April 28, 2021 10:49 AM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -17,46 +17,7 @@ export default class Date extends React.Component {
                        <Text style={styles.text}>NAME: KIM NAMJOON</Text>
                        <Text style={styles.text}>AGE: 26</Text>
                        <Text style={styles.text}>TITLE: ARTIST</Text>
-                       <div class="survey">
-            <fieldset>
-                <legend>Choose a festive beverage</legend>
-            <div>
-                <label for="hot_coco_id">Hot Chocolate</label>
-                <input
-                id="hot_coco_id"
-                type="radio"
-                name="drink_choice"
-                value="Hot Chocolate">
-            </div>
-            <div>
-                <label for="mulled_wine">Mulled Wine</label>
-                <input
-                id="mulled_wine_id"
-                type="radio"
-                name="drink_choice"
-                value="Mulled Wine">
-            </div>
-            <div>
-                <label for="eggnogg_id">Eggnog</label>
-                <input
-                id="eggnog_id"
-                type="radio"
-                name="drink_choice"
-                value="Eggnog">
-            </div>
-            <div>
-                <label for="hot_toddy_id">Hot Toddy</label>
-                <input
-                id="hot_toddy_id"
-                type="radio"
-                name="drink_choice"
-                value="Hot Toddy">
-            </div>
-            <div id="submit_button">
-                <input name="submit" type="submit" class="button" id="submit" value="Click to Submit" />
-            </div>
-            </fieldset>
-        </div>
+                       
                        <Card.Divider/>
                    </SafeAreaView>
                </Card>
