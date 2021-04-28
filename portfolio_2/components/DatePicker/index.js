56d535d3c3bdd7af2c808ab6927a2a136a7a5356
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-27 22:00:46 -0400

    April 27, 2021 10:00 PM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default class App extends React.Component {
+export default class Date extends React.Component {
     constructor(props) {
         super(props)
     }
