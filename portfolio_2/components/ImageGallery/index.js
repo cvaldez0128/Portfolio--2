208a4cee00998dd2a637cd38dce8c3fab68ed5d2
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-27 23:10:25 -0400

    April 27, 2021 11:10 PM

--- portfolio_2/components/ImageGallery/index.js
+++ portfolio_2/components/ImageGallery/index.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+export default class ImageGallery extends React.component {
+    render() {
+        return (
+            <>
+            
+            </>
+        );
+    }
+}
