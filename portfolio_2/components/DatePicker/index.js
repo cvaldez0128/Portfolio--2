3cf5b5f90280905a767b54e970f7f6fe2bf3bd6a
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 11:46:00 -0400

    April 28, 2021 11:46 AM

--- portfolio_2/components/DatePicker/index.js
+++ portfolio_2/components/DatePicker/index.js
@@ -6,98 +6,14 @@ export default class Date extends React.Component {
 
     render() {
         
-        let submitSurvey = () => {
-            if
-        }
-    
+
    
         return (
             <>
     
                <Card>
                    <SafeAreaView style={styles.container}>
-                       <Text style={styles.header}>SURVEY</Text>
-                       <Card.Divider/>
-                       <Text style={styles.text}>
-                            <div class="survey">
-                                <fieldset>
-                                    <legend>Settle the debate of which film is better</legend>
-                                    <div>
-                                        <label for="home_alone_id">Home Alone</label>
-                                        <input
-                                        id="home_alone_id"
-                                        type="radio"
-                                        name="survey_choice"
-                                        value="Home Alone"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label for="home_alone_2_id">Home Alone 2</label>
-                                        <input
-                                        id="home_alone_2_id"
-                                        type="radio"
-                                        name="survey_choice"
-                                        value="Home Alone 2"
-                                        />
-                                    </div>
-                                </fieldset>
-                            </div>
-                       </Text>
-                       <Card.Divider/>
-                       <Text style={styles.text}>
-                            <div class="survey">
-                                <fieldset>
-                                    <legend>Choose a festive beverage</legend>
-                                <div>
-                                    <label for="hot_coco_id">Hot Chocolate</label>
-                                    <input
-                                    id="hot_coco_id"
-                                    type="radio"
-                                    name="drink_choice"
-                                    value="Hot Chocolate"
-                                    />
-                                </div>
-                                <div>
-                                    <label for="mulled_wine">Mulled Wine</label>
-                                    <input
-                                    id="mulled_wine_id"
-                                    type="radio"
-                                    name="drink_choice"
-                                    value="Mulled Wine"
-                                    />
-                                </div>
-                                <div>
-                                    <label for="eggnogg_id">Eggnog</label>
-                                    <input
-                                    id="eggnog_id"
-                                    type="radio"
-                                    name="drink_choice"
-                                    value="Eggnog"
-                                    />
-                                </div>
-                                <div>
-                                    <label for="hot_toddy_id">Hot Toddy</label>
-                                    <input
-                                    id="hot_toddy_id"
-                                    type="radio"
-                                    name="drink_choice"
-                                    value="Hot Toddy"
-                                    />
-                                </div>
-
-
-                                </fieldset>
-                                <Button title="Submit"></Button>
-                            </div>
-
-                       </Text>
-                        <Card.Divider/>
-                        <Text style={styles.text}>
-                            <div>
-                                <p id="survey"></p>
-                                <p id="drink"></p>
-                            </div>    
-                        </Text>                       
+                       
                    </SafeAreaView>
                </Card>
             </>
