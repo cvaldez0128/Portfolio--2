4806e11b3602ef82367c31dc1c8d92bc5a93161d
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 20:13:03 -0400

    April 28, 2021 8:13 PM

--- portfolio_2/components/GrantAccess/index.js
+++ portfolio_2/components/GrantAccess/index.js
@@ -24,6 +24,7 @@ export function VideoPlayer () {
                 isLooping
                 onPlaybackStatusUpdate={status => setStatus(() => status)}
             />
+            <Text style={styles.text}>Video buffering?</Text>
             
             </SafeAreaView>
        </Card>
