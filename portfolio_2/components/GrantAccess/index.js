dc7653b8db2f315679070a18afef45f40a6c0f47
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 22:17:44 -0400

    April 28, 2021 10:17 PM

--- portfolio_2/components/GrantAccess/index.js
+++ portfolio_2/components/GrantAccess/index.js
@@ -52,7 +52,7 @@ export function UserSurvey () {
         if (yesRef.current.value == "Yes") {
             setUserYes(true)
         } else if (yesRef.current.value == "No") {
-            document.getElementsById("mess").innerText = "I'm sorry that the video wasn't your cup of tea. Thanks anyway for watching."
+            document.getElementById("mess").innerHTML = "I'm sorry that the video wasn't your cup of tea. Thanks anyway for watching."
         }
     }
     return (
