c9112fb42da68620a1dd9e3c6464fe0dca097e9f
cvaldez0128 <cvaldez0128@gmail.com>
2021-04-28 20:02:12 -0400

    April 28, 2021 8:02 PM

--- portfolio_2/components/GrantAccess/index.js
+++ portfolio_2/components/GrantAccess/index.js
@@ -63,6 +63,8 @@ export default function passWord() {
                     <>
                         <VideoPlayer></VideoPlayer>
                         <Card.Divider/>
+                        <UserSurvey></UserSurvey>
+                        <Card.Divider/>
                         <Button
                             title="Exit"
                             onPress={back}
